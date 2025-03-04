b8d3c102665982fc54ef0db2c838deb89885c524
Indiwara <indiwarananayakkara1@gmail.com>
2025-03-04 08:23:20 +0530

    hello world

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    //This is a comment
+    /*
+    This
+    is
+    a multiline
+    comment
+    */
+    std::cout << "Hello World" << '\n';
+    std::cout << "Hello how are you today!!" << std::endl;
+    std::cout << "This is \na multi-line \nstatement!" << '\n';
+}
